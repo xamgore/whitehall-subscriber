acbb187d8c6d56f5fb875ac4909c9a23db6b00d9
xamgore <xamgore@ya.ru>
2018-01-07 07:29:45 +0300

    Send notifications in the morning & in the evening

--- src/app.js
+++ src/app.js
@@ -3,14 +3,21 @@ import onExit from './exit'
 import bot from './telegram/bot'
 import db from './whitehall/database'
 import whitehall from './whitehall/broadcast'
+import log from './log'
 
-
-let job = null
-onExit(() => job && job.stop())
 
 db.create()
   .then(() => {
-    job = new cron.CronJob('0 16 21 * * *', whitehall.fetchAndBroadcast)
-    job.start()
+    let jobs = [
+      new cron.CronJob('0 0 11 * * *', whitehall.fetchAndBroadcast),
+      new cron.CronJob('0 0 21 * * *', whitehall.fetchAndBroadcast),
+    ]
+
+    log.i('Register jobs')
+    jobs.forEach(j => j.start())
+    onExit(() => jobs.forEach(j => j.stop()))
   })
-  .then(() => bot.startPolling())
+  .then(() => {
+    log.i('Start polling')
+    bot.startPolling()
+  })
